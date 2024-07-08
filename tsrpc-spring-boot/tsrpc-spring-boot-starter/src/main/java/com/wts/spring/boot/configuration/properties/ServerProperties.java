abec8efd208855ca77437aa7ba7f9c234eb10c29
wts <896517973@qq.com>
2024-07-08 22:20:11 +0800

    Developing the integration for Spring Boot

---
+++ tsrpc-spring-boot/tsrpc-spring-boot-starter/src/main/java/com/wts/spring/boot/configuration/properties/ServerProperties.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2024 wts
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.wts.spring.boot.configuration.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "tsrpc.server")
+public class ServerProperties {
+
+    private Integer port = 6688;
+
+    private Integer bossNum = 1;
+
+    private Integer workerNum = 10;
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
+    }
+
+    public Integer getBossNum() {
+        return bossNum;
+    }
+
+    public void setBossNum(Integer bossNum) {
+        this.bossNum = bossNum;
+    }
+
+    public Integer getWorkerNum() {
+        return workerNum;
+    }
+
+    public void setWorkerNum(Integer workerNum) {
+        this.workerNum = workerNum;
+    }
+}
